dbbaabe885574493c3b6f3de7f4a3819ae5a9e84
Bartłomiej Staroń <bartekstaron@gmail.com>
2022-05-27 16:13:37 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          cos tu bedzie
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
